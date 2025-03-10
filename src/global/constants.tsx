affbf4a67bc5d602ebc2d3cb00e0638ebe9a7ed2
piyush97 <me@piyushmehta.com>
2025-03-10 17:02:10 -0400

    Add application name and description constants; integrate Toaster in layout

---
+++ src/global/constants.tsx
@@ -0,0 +1,3 @@
+export const APP_NAME = "Pitch Perfect - AI based Presentation Tool";
+export const APP_DESCRIPTION =
+  "Pitch Perfect is an AI based Presentation Tool that helps you create engaging presentations.";
