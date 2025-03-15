2bc0c5d1f92236fe3ac806267e465de4fd326216
piyush97 <me@piyushmehta.com>
2025-03-14 20:21:26 -0400

    [WIP] type eslint check ignore

--- src/components/global/projects/project-card.tsx
+++ src/components/global/projects/project-card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { itemVariant, ROUTES, themes } from "@/global/constants";
 import { useSlideStore } from "@/store/useSlideStore";
