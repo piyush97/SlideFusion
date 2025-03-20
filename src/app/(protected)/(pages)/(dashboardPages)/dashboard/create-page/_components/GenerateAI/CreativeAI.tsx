b3801dc12f83e3aae62bd8d0b879238a743feaf2
Piyush Mehta <me@piyushmehta.com>
2025-03-20 18:58:08 -0400

    add select input for number of cards and integrate AI prompt state management

--- src/app/(protected)/(pages)/(dashboardPages)/dashboard/create-page/_components/GenerateAI/CreativeAI.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/dashboard/create-page/_components/GenerateAI/CreativeAI.tsx
@@ -2,17 +2,27 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { containerVariants, itemVariant } from "@/global/constants";
+import { useCreativeAIStore } from "@/store/useCreativeAIStore";
 import { motion } from "framer-motion";
 import { ChevronLeft } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 type Props = {
   onBack: () => void;
 };
 
 const CreativeAI = ({ onBack }: Props) => {
-  const router = useRouter();
+  const [noOfCards, setNoOfCards] = useState(0);
+  const { currentAIPrompt, setCurrentAIPrompt, outlines } =
+    useCreativeAIStore();
 
   return (
     <motion.div
@@ -38,8 +48,42 @@ const CreativeAI = ({ onBack }: Props) => {
           <Input
             placeholder="Enter prompt and add to the cards..."
             className="text-base sm:text-xl border-0 focus-visible:ring-0 shadow-none p-0 bg-transparent flex-grow"
+            value={currentAIPrompt || ""}
+            onChange={(e) => {
+              setCurrentAIPrompt(e.target.value);
+            }}
             required
           />
+          <div className="flex items-center gap-3">
+            <Select
+              value={noOfCards.toString()}
+              onValueChange={(value) => setNoOfCards(parseInt(value))}
+            >
+              <SelectTrigger className="w-fit gap-2 font-semibold shadow-xl">
+                <SelectValue placeholder="Select number of Cards" />
+              </SelectTrigger>
+              <SelectContent className="w-fit">
+                {outlines.length === 0 ? (
+                  <SelectItem value="0" className="font-semibold">
+                    {" "}
+                    No Cards{" "}
+                  </SelectItem>
+                ) : (
+                  Array.from({ length: outlines.length }, (_, i) => i + 1).map(
+                    (num) => (
+                      <SelectItem
+                        key={num}
+                        value={num.toString()}
+                        className="font-semibold"
+                      >
+                        {num} Card{num > 1 ? "s" : ""}
+                      </SelectItem>
+                    )
+                  )
+                )}
+              </SelectContent>
+            </Select>
+          </div>
         </div>
       </motion.div>
     </motion.div>
