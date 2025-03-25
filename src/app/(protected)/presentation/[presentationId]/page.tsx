5abf78474297a23201b250c09a523b69cbf32e45
Piyush Mehta <me@piyushmehta.com>
2025-03-24 20:58:26 -0400

    Add react-dnd and update presentation page component
    - Added react-dnd and react-dnd-html5-backend dependencies
    - Updated presentation page component to use DndProvider
    - Removed unused imports and variables

--- src/app/(protected)/presentation/[presentationId]/page.tsx
+++ src/app/(protected)/presentation/[presentationId]/page.tsx
@@ -1,31 +1,31 @@
 import { getProjectById } from "@/actions/project";
 import { themes } from "@/global/constants";
+import { useSlideStore } from "@/store/useSlideStore";
 import { Loader2 } from "lucide-react";
-import { useTheme } from "next-themes";
 import { redirect, useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { DndProvider } from "react-dnd";
 import { toast } from "sonner";
 
-const Page = (props: Props) => {
-  const { setSlides, setProject, currentTheme, setCurrentTheme } =
-    useSlideStore();
+const Page = () => {
+  const { setSlides, setProject, setCurrentTheme } = useSlideStore();
   const params = useParams();
-  const { setTHeme } = useTheme();
+  // const { setTheme } = useTheme();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       try {
-        if (params.id) {
-          const res = await getProjectById(params?.presentationId as string);
-          if (res.status !== 200 || !res.data) {
-            toast.error("Failed to fetch project");
-          }
+        const res = await getProjectById(params?.presentationId as string);
+        if (res.status !== 200 || !res.data) {
+          toast.error("Failed to fetch project");
           redirect("/dashboard");
         }
+
         const findTheme = themes.find(
           (theme) => theme.name === res?.data.themeName,
         );
+
         setCurrentTheme(findTheme || themes[0]);
         setProject(res.data);
         setSlides(JSON.parse(JSON.stringify(res?.data.slides)));
@@ -45,5 +45,7 @@ const Page = (props: Props) => {
       </div>
     );
 
-  return <div>{/* Your component content */}</div>;
+  return <DndProvider>{/* Your component content */}</DndProvider>;
 };
+
+export default Page;
