668b8827f5eef7efed2ffca3bca8f159a2a83347
Piyush Mehta <me@piyushmehta.com>
2025-04-29 21:32:20 -0400

    Adds right sidebar with layout selector popover
    Introduces a new right-side editor sidebar featuring a popover layout selector.
    Enhances user experience by providing quick access to layout options styled with the current theme.

---
+++ src/app/(protected)/presentation/[presentationId]/_components/right-sidebaar/tabs/LayoutSelector.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useSlideStore } from "@/store/useSlideStore";
+
+type Props = {};
+
+const LayoutSelector = (props: Props) => {
+  const { currentTheme } = useSlideStore();
+  return (
+    <ScrollArea
+      className="h-[400px]"
+      style={{ backgroundColor: currentTheme.slideBackgroundColor }}
+    >
+      {" "}
+      LayoutSelector
+    </ScrollArea>
+  );
+};
+
+export default LayoutSelector;
