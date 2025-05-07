e1747feb616bc5df41605bb255031c03bda9550d
Piyush Mehta <me@piyushmehta.com>
2025-05-06 23:07:33 -0400

    Adds bulk delete for projects with user feedback
    Introduces a function to delete multiple projects at once, including validation and authentication. Updates the UI to allow users to trigger bulk deletion and receive success or error toasts. Improves user experience by streamlining project cleanup.

--- src/app/(protected)/(pages)/(dashboardPages)/dashboard/trash/_components/DeleteAllButton.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/dashboard/trash/_components/DeleteAllButton.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { deleteAllProjects } from "@/actions/project";
 import AlertDialogBox from "@/components/global/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Project } from "@prisma/client";
 import { Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
 
 type Props = { Projects: Project[] };
 
@@ -12,6 +14,30 @@ const DeleteAllButton = ({ Projects }: Props) => {
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+
+  const handleDeleteAllProjects = async () => {
+    setLoading(true);
+    if (!Projects || Projects.length === 0) {
+      setLoading(false);
+      toast.error("Error: No projects found");
+      return;
+    }
+
+    try {
+      const res = await deleteAllProjects(
+        Projects.map((project) => project.id)
+      );
+
+      if (res.status !== 200) {
+        toast.error(`Something is wrong, please try again later: ${res.error}`);
+        return;
+      }
+      toast.success("All projects deleted successfully");
+    } catch (error) {
+      console.error("Error deleting all projects", error);
+      toast.error("Something went wrong, please try again later");
+    }
+  };
 
   return (
     <AlertDialogBox
