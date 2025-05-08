8c287d9e830b399dba153a80f539eba66c4c8365
Piyush Mehta <me@piyushmehta.com>
2025-05-07 22:52:20 -0400

    Enhances settings page with user auth and UI layout
    Introduces user authentication check on the settings page and replaces placeholder with a structured layout, including headings and descriptive text for improved user experience.

--- src/app/(protected)/(pages)/(dashboardPages)/dashboard/settings/page.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/dashboard/settings/page.tsx
@@ -1,7 +1,24 @@
+import { onAuthenticateUser } from "@/actions/user";
+
 type Props = {};
 
 const Page = (props: Props) => {
-  return <div>Page</div>;
+  const checkUser = await onAuthenticateUser();
+
+  return (
+    <div className="relative flex-col gap-6 div">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col items-start">
+          <h1 className="text-2xl font-semibold dark:text-primary backdrop-blur-lg">
+            Settings
+          </h1>
+          <p className="text-base font-normal dark:text-secondary">
+            All your settings
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
