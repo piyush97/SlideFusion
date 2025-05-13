ddc557daab86ad5d230db6926fabb4ad01b65714
Piyush Mehta <me@piyushmehta.com>
2025-05-13 19:57:53 -0400

    Adds webhook signature validation and updates subscription
    Implements HMAC-based signature verification for incoming webhook
    requests to enhance security and prevent spoofing. Updates the
    user's subscription status in the database upon successful
    validation and provides clearer error handling for invalid
    signatures and update failures.

--- src/app/api/webhook/subscription/route.ts
+++ src/app/api/webhook/subscription/route.ts
@@ -1,6 +1,7 @@
-import { NextRequest } from "next/server";
-
 export const dynamic = "force-dynamic"; // to remove cache
+import { client } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+import crypto from "node:crypto";
 
 export async function POST(req: NextRequest) {
   try {
@@ -12,7 +13,52 @@ export async function POST(req: NextRequest) {
     if (!buyerUserId) {
       throw new Error("Missing buyerUserId in custom_data");
     }
+
+    const hmac = crypto.createHmac(
+      "sha256",
+      process.env.LEMON_SQUEEZY_WEBHOOK_SECRET!
+    ); // Create HMAC using the secret key
+
+    const digest = Buffer.from(hmac.update(rawBody).digest("hex"), "utf8"); // Create HMAC digest
+
+    const signature = Buffer.from(req.headers.get("x-signature")!, "utf8"); // Get the signature from the request header
+
+    console.log("Digest:", digest);
+    console.log("Signature:", signature);
+
+    // prevent timing attack
+    if (!crypto.timingSafeEqual(digest, signature)) {
+      throw new Error("Invalid signature");
+    }
+
+    // update the order status in the database
+
+    const buyer = await client.user.update({
+      where: {
+        id: buyerUserId,
+      },
+      data: {
+        subscription: true,
+      },
+    });
+
+    if (!buyer) {
+      return Response.json({
+        message: "Cannot update the subscription",
+        status: 404,
+      });
+    }
+
+    return Response.json(
+      {
+        message: "Subscription updated successfully",
+        data: buyer,
+        status: 200,
+      },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error processing webhook:", error);
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
   }
 }
